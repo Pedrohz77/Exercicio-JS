67cb3aa14156a8898e93ff302a5ae41091611a5a
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-02 17:12:19 -0300

    feat: Exemplo

---
+++ script.js
@@ -0,0 +1,26 @@
+// Escreve um programa que solicite o nome de usúario e lhe dê boas vindas,
+// exibindo uma mengagem pelo console,
+
+// prompt -> Recebe uma entrada digitada 
+let nome = prompt("Olá, digite seu nome: ")
+
+// Concatenação
+// console.log("Seja bem vindo " + nome + "!!!");
+
+let pontos = 100 
+
+// Adicao -> num1 + num2
+// Subtração -> num1 - num2
+// Multiplicação -> num1 * num2
+// Divisão -> num1 / num2
+
+//
+let bonus = parseInt( prompt("Quantos pontos você deseja somar") )
+
+let pontuacaoTotal = pontos + bonus
+console.log(pontos + bonus)
+
+// parseFloat() -> Transformar= o que está entre () em número com, (quebr)
+let notaAluno = parseFloat(prompt ("Digite a nota do aluno:" ) )
+
+console.log(notaAluno)
