f604626543e99a631c1fceebf8517d2e5fc68833
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-07 17:19:16 -0300

    feat: Exercicio 4 feito

---
+++ Loop/ex4.js
@@ -0,0 +1,8 @@
+// Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+
+let num = parseInt (prompt ("Digite um número de 1 a 10"));
+
+for (let tabuada = 1; tabuada <=10; tabuada++){
+    console.log (tabuada * num)
+}
