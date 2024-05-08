b7474efe0f3f4a1e525e2d98538084cf149b790e
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-08 15:27:51 -0300

    feat: Exercicio 9 feito

---
+++ Loop/ex9.js
@@ -0,0 +1,10 @@
+// Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+
+let usuario = parseInt (prompt ("Digite um número: "))
+
+while (usuario >= 1) {
+
+    usuario --;
+    console.log (usuario)
+}
