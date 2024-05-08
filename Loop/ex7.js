1698c82238b7b1dcb041478816a4bca08e5f3642
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-08 15:00:59 -0300

    feat: Exercicio 6 feito

---
+++ Loop/ex7.js
@@ -0,0 +1,11 @@
+// Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let soma = 0
+let num1 = 1
+
+while (num1 <= 100) {
+
+    console.log (soma = soma + num1)
+    num1 ++;
+    
+}
