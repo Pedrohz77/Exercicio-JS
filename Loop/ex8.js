630391ca44afc35e20f5867525069bdd03a8616e
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-08 15:12:21 -0300

    feat: Exercicio 8 feito

---
+++ Loop/ex8.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+
+let idade = 1
+
+while (idade < 18) {
+    idade= parseInt (prompt ("Digite sua idade: "))
+}
+
+console.log ("Você é maior de idade")
