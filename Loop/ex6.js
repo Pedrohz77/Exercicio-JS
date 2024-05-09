86e5ba21df90d407b5e0d0b78ed70bf4b80def91
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-09 14:38:40 -0300

    feat: Exercicio 7 feito

--- Loop/ex6.js
+++ Loop/ex6.js
@@ -1,18 +1,10 @@
 // Imprimir números pares de 0 a 20 utilizando o loop while
 
 let par = 0;
-
-while (par < 20) {
-    par += 2;
-
-    console.log (par);
+   
+while (par <= 20) {
+    if(par % 2 == 0) {
+        console.log(par)
+    }
+    num ++;
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
--- Loop/ex7.js
+++ Loop/ex7.js
@@ -4,8 +4,9 @@ let soma = 0
 let num1 = 1
 
 while (num1 <= 100) {
-
-    console.log (soma = soma + num1)
-    num1 ++;
+    soma += num1;
+    num1++;
     
 }
+
+console.log("A soma dos números de 1 a 100 é: " + soma);
