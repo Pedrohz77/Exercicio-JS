8acaa789c923627033c96e8947e522109888adb1
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-09 14:58:21 -0300

    feat: Exercicio 10 feito

--- Loop/ex10.js
+++ Loop/ex10.js
@@ -1,11 +1,11 @@
 // Faça um programa que receba um número do usuário e continue
 // a pedir o número se o numero for diferente de 0.
 
-let num = 0
+let num;
 
 do {
-    num = parseInt (prompt ("Digite um número: "))
+    num = parseInt (prompt ("Digite o número 0 pra sair"))
     
-}while (num != 0)
+}while (num !== 0)
 
-console.log (num)
+console.log ("Vocé digitou 0, o programa foi encerrado");
