6dd0320993f25c70c8a82cf3ac4dd5aefee88732
Pedrohzx7 <https://github.com/Pedrohzx7>
2024-05-09 17:04:49 -0300

    feat: Exercicio 6 feito

--- Loop/ex6.js
+++ Loop/ex6.js
@@ -1,10 +1,12 @@
 // Imprimir números pares de 0 a 20 utilizando o loop while
 
 let par = 0;
-   
-while (par <= 20) {
-    if(par % 2 == 0) {
+
+console.log("Números pares de 0 a 20: ");
+
+while(par <= 20){
+    if(par % 2 == 0){
         console.log(par)
     }
-    num ++;
+    par ++;
 }
